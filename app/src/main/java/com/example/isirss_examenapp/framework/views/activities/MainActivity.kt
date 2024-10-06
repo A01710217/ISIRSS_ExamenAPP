7ce664888909ef83e3d325677d4e17e40aff2eb1
A01710217 <a01710217@tec.mx>
2024-10-06 13:44:37 -0600

    feat(): crear los patrones de arquitectura (MVVM y clean)

---
+++ app/src/main/java/com/example/isirss_examenapp/framework/views/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.isirss_examenapp.framework.views.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.isirss_examenapp.R
+
+class MainActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/example/isirss_examenapp/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.isirss_examenapp.utils
+
+object Constants {
+    const val SPLASHCREEN_DURATION = 3000L
+}
