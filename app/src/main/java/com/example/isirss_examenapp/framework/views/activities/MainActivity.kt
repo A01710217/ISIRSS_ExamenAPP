b9db94bce406da1657aaba5858fcf823211abb7a
A01710217 <a01710217@tec.mx>
2024-10-06 14:02:11 -0600

    style(): agregar colores y mostrar layaut

--- app/src/main/java/com/example/isirss_examenapp/framework/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/isirss_examenapp/framework/views/activities/MainActivity.kt
@@ -5,8 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.isirss_examenapp.R
+import com.example.isirss_examenapp.databinding.ActivityMainBinding // Asegúrate de que la ruta sea correcta
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Inicializar el binding
+        initializeBinding()
+    }
+
+    private fun initializeBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+    }
 }
